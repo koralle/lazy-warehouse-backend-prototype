95b167a27b9dda9cc338ff3b719079a47e4900f5
koralle <mkhd7930@gmail.com>
2023-03-01 23:59:21 +0900

    fix: main.go

--- main.go
+++ main.go
@@ -1,21 +1,47 @@
 package main
 
 import (
+	"context"
 	"fmt"
-	"net/http"
+	"log"
+	"net"
 	"os"
+
+	"github.com/koralle/lazy-warehouse-backend-prototype/config"
+	"github.com/koralle/lazy-warehouse-backend-prototype/mux"
+	"github.com/koralle/lazy-warehouse-backend-prototype/server"
 )
 
-func main() {
-	err := http.ListenAndServe(
-		":18080",
-		http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			fmt.Fprintf(w, "Hello, %s!", r.URL.Path[1:])
-		}),
-	)
+func run(ctx context.Context) error {
+	cfg, err := config.New()
+	if err != nil {
+		return err
+	}
+
+	l, err := net.Listen("tcp", fmt.Sprintf(":%d", cfg.Port))
+	if err != nil {
+		log.Fatalf("failed to listen port %d: %v", cfg.Port, err)
+	}
+
+	url := fmt.Sprintf("http://%s", l.Addr().String())
+	log.Printf("start with: %v", url)
+
+	mux, cleanup, err := mux.NewMux(ctx, cfg)
 
 	if err != nil {
-		fmt.Printf("failed to terminate server: %v", err)
+		return err
+	}
+
+	defer cleanup()
+
+	s := server.NewServer(l, mux)
+
+	return s.Run(ctx)
+}
+
+func main() {
+	if err := run(context.Background()); err != nil {
+		log.Printf("failed to terminate server: %v", err)
 		os.Exit(1)
 	}
 }
