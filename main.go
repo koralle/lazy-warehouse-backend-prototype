5e3d4317a78a1e4276f0940c7beb4de49f1a096e
koralle <mkhd7930@gmail.com>
2023-03-01 22:22:44 +0900

    fix: かんたんなwebサーバーを実装

--- main.go
+++ main.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+	"os"
+)
 
 func main() {
-  fmt.Println("Hello, world!")
+	err := http.ListenAndServe(
+		":18080",
+		http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			fmt.Fprintf(w, "Hello, %s!", r.URL.Path[1:])
+		}),
+	)
+
+	if err != nil {
+		fmt.Printf("failed to terminate server: %v", err)
+		os.Exit(1)
+	}
 }
