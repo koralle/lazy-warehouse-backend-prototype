706a8f8da302b33d86b547d1d78d3e8e6cbb9e5d
koralle <mkhd7930@gmail.com>
2023-03-02 22:33:52 +0900

    add: Serverパッケージを作ってサーバーの起動コードを書き直し

--- main.go
+++ main.go
@@ -1,20 +1,31 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
-	"net/http"
+	"net"
 	"os"
 
 	"github.com/koralle/lazy-warehouse-backend-prototype/config"
 	"github.com/koralle/lazy-warehouse-backend-prototype/mux"
+	"github.com/koralle/lazy-warehouse-backend-prototype/server"
 )
 
-func run() error {
+func run(ctx context.Context) error {
 	config, err := config.New()
 	if err != nil {
 		return err
 	}
+
+	l, err := net.Listen("tcp", fmt.Sprintf(":%d", config.Port))
+	if err != nil {
+		log.Fatalf("failed to listen port %d: %v", config.Port, err)
+	}
+
+	url := fmt.Sprintf("http://%s", l.Addr().String())
+	log.Printf("start with: %v", url)
+
 	mux, cleanup, err := mux.NewMux()
 
 	if err != nil {
@@ -23,13 +34,14 @@ func run() error {
 
 	defer cleanup()
 
-	http.ListenAndServe(fmt.Sprintf(":%d", config.Port), mux)
+	s := server.NewServer(l, mux)
 
-	return nil
+	return s.Run(ctx)
+
 }
 
 func main() {
-	if err := run(); err != nil {
+	if err := run(context.Background()); err != nil {
 		log.Printf("failed to terminate server: %v", err)
 		os.Exit(1)
 	}
